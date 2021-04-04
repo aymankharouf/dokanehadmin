9f9ac111e22bf9810f7df1b02b9c9ee925b087e5
ayman <ayman.wkh@gmail.com>
2021-04-04 06:55:43 +0300

    upgrade to framework7 v6

--- src/app.js
+++ src/app.js
@@ -2,21 +2,20 @@ import { App, Panel, View } from 'framework7-react'
 import routes from './routes'
 import Store from './data/store'
 
-const app = props => {
-  const f7params = {
-    id: 'io.framework7.dokaneh',
-    name: 'دكانة نت',
-    theme: 'auto', 
-    routes,
-  }
-
+const f7params = {
+  id: 'io.framework7.dokaneh', 
+  name: 'Dokkaneh', 
+  theme: 'ios',
+  routes,
+}
+const app = () => {
   return (
     <Store>
-      <App params={f7params}>
+      <App {...f7params}>
         <Panel right reveal themeDark>
           <View url="/panel/"/>
         </Panel>
-          <View id="main-view" url="/" main className="safe-areas" pushState={true}/>
+        <View id="main-view" url="/" main className="safe-areas" browserHistory={true}/>
       </App>
     </Store>
   )
